c6676ef63f012ae31b7353dc4601634e5a063fc8
Munyorodee <munyoro.md95@gmail.com>
2022-06-08 00:31:17 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <unistd>
-#include <main.h>
-gcc main.c
+#include <unistd.h>
+#include "main.h"
 
-/*AUTH. MUNYORO MUFARO
-/* main - Prints _puthar followed by a new line. Return: Always 0*/
+/**
+ *  * main - Prints "_putchar" followed by a new line.
+ *   *
+ *    * Return: Always 0.
+ *     */
 
 int main(void)
-
 {
-  	_putchar('_');
-    	_putchar('p');
-    	_putchar('u');
-    	_putchar('t');
-    	_putchar('c');
-    	_putchar('h');
-    	_putchar('a');
-    	_putchar('r');
-    	_putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+										_putchar('\n');
 
-    										return (0);
-	
-	}
+											return (0);
+}
+
