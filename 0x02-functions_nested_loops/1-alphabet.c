152787f4563b241678b05f8b2d906021fb4f7c69
Munyorodee <munyoro.md95@gmail.com>
2022-06-08 05:10:29 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/*
+ * * Author:munyoro mufaro
+ * */
+
+#include "main.h"
+
+/**
+ * * print_alphabet - function prototype to print alphabet in lowercase,
+ * * followed by a new line
+ * */
+
+void print_alphabet(void)
+{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar('\n');
+	
+}
