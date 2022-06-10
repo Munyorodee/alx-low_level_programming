5ae4bb10ea8141ebaea92cb4f875bc2d0d289eef
MUNYORO MD <105204048+Munyorodee@users.noreply.github.com>
2022-06-10 22:02:08 +0200

    My commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/*
+* Auth: Munyoro Mufaro David
+*/
+
+#include "main.h"
+
+/**
+ * mul - Multiples of two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication of int a and in b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
