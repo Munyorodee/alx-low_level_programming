d1b873b06410158b9211d246473a6f114007fc01
Munyorodee <munyoro.md95@gmail.com>
2022-06-22 06:40:50 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+int _pow_recursion(int x, int y)
+{
+		if y < 0
+				return(-1);
+
+		else
+				return(x*_pow(y));
+
+		if x == 0
+				return(0);
+
+		else
+				return(x*_pow_recursion
