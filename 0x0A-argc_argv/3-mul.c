327508afca6c743beb70e901d8d33e1472c1467b
Munyorodee <munyoro.md95@gmail.com>
2022-06-24 05:02:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.c>
+
+int main(int argc, char *argv[])
+{
+		int a
+		int b
+
+		if argc == 3
+		{		
+				int c = a*b
+				printf("%d\n", c);
+		}
+		else
+		{
+				printf("Error\n");
+				return(1);
+		}
+		return (0);
+}
+
